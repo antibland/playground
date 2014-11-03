1a9d302c2eba52b8a30f95284e2ac63a51e28036
Andy Hoffman <andy@mobilebandaid.com>
2014-11-02 23:19:15 -0500

    initial shot at fancy questionnaire

---
+++ questionnaire/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+  {
+    question: "Would you rather be a groundhog or a bumblebee? <strong>Keep in mind</strong> that it doesn't really matter what you say. There is a right answer and you won't get it.",
+    options: [
+      "Groundhog all the way",
+      "Bumblebee because BEES",
+      "I refuse to answer this shit."
+    ]
+  },
+  {
+    question: "Would you rather be a red hound or a brilliant shark? You should answer these sorts of questions <em>briskly</em> as the sun is slowly blossoming into the Earth.",
+    options: [
+      "Groundhog all the way",
+      "Bumblebee because BEES"
+    ]
+  },
+  {
+    question: "Would you rather be a fruit bat or a fruit bar? <strong>Keep in mind</strong> that it doesn't really matter what you say. There is a right answer and you won't get it.",
+    options: [
+      "Groundhog all the way",
+      "Bumblebee because BEES",
+      "I refuse to answer this shit."
+    ]
+  },
+  {
+    question: "Would you rather be an amazing primate or a stupid person? You should answer these sorts of questions <em>briskly</em> as the sun is slowly blossoming into the Earth.",
+    options: [
+      "Groundhog all the way",
+      "Bumblebee because BEES",
+      "I refuse to answer this shit."
+    ]
+  }
+];
